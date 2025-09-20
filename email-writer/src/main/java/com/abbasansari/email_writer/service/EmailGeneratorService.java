afe3ae6b8b40155667dc849b0655e6932f8dd7a5
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-20 22:09:28 +0530

    implement EmailGeneratorService for AI-powered email replies via Gemini API
    - Added generateEmailReply to send email content and tone to Gemini API.
    - Built dynamic prompt generator for tone customization.
    - Parsed API response using Jackson to extract reply text.
    - Configured WebClient with externalized API URL and key.
    - Added exception handling for response parsing errors.

--- email-writer/src/main/java/com/abbasansari/email_writer/service/EmailGeneratorService.java
+++ email-writer/src/main/java/com/abbasansari/email_writer/service/EmailGeneratorService.java
@@ -1,8 +1,77 @@
 package com.abbasansari.email_writer.service;
 
+import com.abbasansari.email_writer.model.EmailRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
+    private final WebClient webClient;
 
+    @Value("${gemini.api.url}")
+    private String GEMINI_API_URL;
+
+    @Value("${gemini.api.key}")
+    private String GEMINI_API_KEY;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public  String generateEmailReply(EmailRequest emailRequest){
+//        Build the prompt
+        String prompt = buildPrompt(emailRequest);
+
+//        Craft a request
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", prompt)
+                        })
+                }
+        );
+//        Do request & Get response
+        String response = webClient.post()
+                .uri(GEMINI_API_URL + GEMINI_API_KEY)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+//       Extract response & Return
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+
+        }catch (Exception e){
+            return "Error processing request: " + e.getMessage();
+        }
+    }
+
+    private String buildPrompt(EmailRequest emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional reply for the following email content. please don't generate a subject line");
+        if(emailRequest.getTone()!= null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ").append(emailRequest.getTone()).append(" tone.");
+        }
+        prompt.append("\n Original email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
+    }
 }
